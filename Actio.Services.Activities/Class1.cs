1c2bb1f49d261a3c33c71269d39945a3954693b6
Umberto <Umbertogotti1979@gmail.com>
2019-01-31 21:50:54 +0100

    Initial project skeleton created.

---
+++ Actio.Services.Activities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Actio.Services.Activities
+{
+    public class Class1
+    {
+    }
+}
---
+++ Actio.Services.Identity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Actio.Services.Identity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
